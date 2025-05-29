a47f5c7524fbe53d60553b725f4b0adf752dd584
Miguel <miguel.fedrigo@escola.pr.gov.br>
2025-05-29 08:27:23 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(246,120,40);
+  fill(0,0,255)
+  stroke(0,0,0)
+  strokeWeight(2)
+  circle(200,200,250)
+  fill(255,255,255)
+  circle(150,150,50)
+  circle(250,150,50)
+  line(120, 120,170,120);
+  line(220, 250,180,250);
+  line(220,120,273,120)
+  fill(0,255,255)
+  triangle(220, 235, 200, 200, 180,235)
+  
+  fill(255,105,180)
+  circle(150,150,25)
+  circle(250,150,25)
+  fill(0,0,0)
+  circle(150,150,10)
+  circle(250,150,10)
+}
